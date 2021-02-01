92ae41152f001952083cb3cae78c7d8cc26324dc
4scencio <alessandroascencio@protonmail.com>
2021-02-01 08:31:41 -0800

    assets/css

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Salve CARALHOOOO</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+  },
+  data() {
+    return {
+      name: 'Mama meu saco'
+    }
+  }
+})
+</script>
