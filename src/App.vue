c26578a68aa3c32b616cb676f5debad6ad80be03
4scencio <alessandroascencio@protonmail.com>
2021-02-01 08:39:04 -0800

    delete component test

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header />
-  <Header/>
+  <h1>Olá</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Header from '@/components/Header.vue'
 
 export default defineComponent({
-  components: { Header },
+  
   data() {
     return {
       name: 'Alessandro'
